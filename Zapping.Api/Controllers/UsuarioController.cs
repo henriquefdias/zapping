b245f2821890747ce0b2659061635c6f03bb389e
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-10-15 07:15:12 -0300

    Autenticacao

--- Zapping.Api/Controllers/UsuarioController.cs
+++ Zapping.Api/Controllers/UsuarioController.cs
@@ -11,7 +11,9 @@ using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
+using Zapping.Api.Security;
 using Zapping.Domain.Commands.Usuario.AdicionarUsuario;
+using Zapping.Domain.Commands.Usuario.AutenticarUsuario;
 using Zapping.Infra.Repositories.Transactions;
 
 namespace Zapping.Api.Controllers
@@ -30,7 +32,6 @@ namespace Zapping.Api.Controllers
         [Route("Adicionar")]
         public async Task<IActionResult> Adicionar([FromBody]AdicionarUsuarioRequest request)
         {
-
             try
             {
                 var response = await _mediator.Send(request, CancellationToken.None);
@@ -39,6 +40,80 @@ namespace Zapping.Api.Controllers
             catch (System.Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("Autenticar")]
+        public async Task<IActionResult> Autenticar(
+           [FromBody] AutenticarUsuarioRequest request,
+           [FromServices] SigningConfigurations signingConfigurations,
+           [FromServices] TokenConfigurations tokenConfigurations)
+        {
+
+            try
+            {
+                var autenticarUsuarioResponse = await _mediator.Send(request, CancellationToken.None);
+
+                if (autenticarUsuarioResponse.Autenticado == true)
+                {
+                    var response = GerarToken(autenticarUsuarioResponse, signingConfigurations, tokenConfigurations);
+
+                    return Ok(response);
+                }
+
+                return Ok(autenticarUsuarioResponse);
+
+            }
+            catch (System.Exception ex)
+            {
+
+                return NotFound(ex.Message);
+            }
+        }
+
+        private object GerarToken(AutenticarUsuarioResponse response, SigningConfigurations signingConfigurations, TokenConfigurations tokenConfigurations)
+        {
+            if (response.Autenticado == true)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    new GenericIdentity(response.Id.ToString(), "Id"),
+                    new[] {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        //new Claim(JwtRegisteredClaimNames.UniqueName, response.Usuario)
+                        new Claim("Usuario", JsonConvert.SerializeObject(response))
+                    }
+                );
+
+                DateTime dataCriacao = DateTime.Now;
+                DateTime dataExpiracao = dataCriacao + TimeSpan.FromSeconds(tokenConfigurations.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+                {
+                    Issuer = tokenConfigurations.Issuer,
+                    Audience = tokenConfigurations.Audience,
+                    SigningCredentials = signingConfigurations.SigningCredentials,
+                    Subject = identity,
+                    NotBefore = dataCriacao,
+                    Expires = dataExpiracao
+                });
+                var token = handler.WriteToken(securityToken);
+
+                return new
+                {
+                    authenticated = true,
+                    created = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expiration = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    accessToken = token,
+                    message = "OK",
+                    PrimeiroNome = response.Nome
+                };
+            }
+            else
+            {
+                return response;
             }
         }
 
